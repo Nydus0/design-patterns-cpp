d34be2706eeffaaedd4cd5675c114b0bf5607b3c
Nydus0 <loicplantin@orange.fr>
2025-07-09 21:33:48 +0200

    fix: object pool minor corrections

--- object_pool/ObjectPool.hpp
+++ object_pool/ObjectPool.hpp
@@ -29,55 +29,28 @@ class ObjectPool {
        _freeObjects and _storedObjects.
        **/
 public:
-    explicit ObjectPool(size_t poolSize) : MAX_POOL_SIZE(poolSize) {
-    }
+    explicit ObjectPool(size_t poolSize) : _maxPoolSize(poolSize) {}
 
-    T *storeObject(Key key) {
-        auto availableObject = move(_freeObjects.top());
-        _freeObjects.pop();
-        T *resultPtr = availableObject.get();
-        _storedObjects.insert({key, move(availableObject)});
-        return resultPtr;
-    }
+    [[nodiscard]] T *acquire(const Key &key) {
 
-    T *acquire(const Key &key) {
-        T *resultPtr;
-
-        _currentSize = _storedObjects.size() + _freeObjects.size();
-
-        if (_currentSize < MAX_POOL_SIZE) {
-            //maximum object number has not yet been reached
-
-            if (!_freeObjects.empty()) {
-                //free objects are available => get one free object and add it to stored objects
-                auto availableObject = move(_freeObjects.top());
-                _freeObjects.pop();
-                resultPtr = availableObject.get();
-                _storedObjects.insert({key, move(availableObject)});
-            } else {
-                //no free objects available => construct and store directly in stored objects
-                auto newObject = std::make_unique<T>();
-                resultPtr = newObject.get();
-                _storedObjects.insert({key, move(newObject)});
-            }
-        } else {
-            //maximum object number is reached
-
-            if (!_freeObjects.empty()) {
-                //free objects are available => get one free object and add it to stored objects
-                auto availableObj = move(_freeObjects.top());
-                _freeObjects.pop();
-                resultPtr = availableObj.get();
-                _storedObjects.insert({key, move(availableObj)});
-            } else {
-                //no more free objects available
-                resultPtr = nullptr;
-            }
+        if (_storedObjects.size() + _freeObjects.size() >= _maxPoolSize && _freeObjects.empty()) {
+            return nullptr;
         }
+
+        std::unique_ptr<T> obj;
+        if (!_freeObjects.empty()) {
+            obj = std::move(_freeObjects.top());
+            _freeObjects.pop();
+        } else {
+            obj = std::make_unique<T>();
+        }
+
+        T* resultPtr = obj.get();
+        _storedObjects.emplace(key, std::move(obj));
         return resultPtr;
     }
 
-    T *get(const Key &key) {
+    [[nodiscard]] T *get(const Key &key) {
         auto it = _storedObjects.find(key);
         if (it != _storedObjects.end()) {
             return it->second.get();
@@ -88,17 +61,18 @@ public:
 
     std::vector<T *> getObjects() {
         std::vector<T *> vec;
-        for (auto &it: _storedObjects) {
-            vec.push_back(it.second.get());
+        vec.reserve(_storedObjects.size());
+        for (auto& [key, obj] : _storedObjects) {
+            vec.push_back(obj.get());
         }
         return vec;
     }
 
     void release(const Key &key) {
         auto it = _storedObjects.find(key);
-        if (it != _storedObjects.end()) {
+        if (it != _storedObjects.end() && it->second) {
             it->second->reset();
-            _freeObjects.push(move(it->second)); //transfer ownership to free objects stack
+            _freeObjects.push(std::move(it->second)); //transfer ownership to free objects stack
             _storedObjects.erase(it); //erase element from map
         }
     }
@@ -111,13 +85,12 @@ public:
         _storedObjects.clear();
     }
 
-    bool isAtCapacity() {
-        return _storedObjects.size() >= MAX_POOL_SIZE;
+    [[nodiscard]] bool isAtCapacity() const {
+        return _storedObjects.size() >= _maxPoolSize;
     }
 
 private:
-    size_t MAX_POOL_SIZE{0};
-    size_t _currentSize{0};
+    size_t _maxPoolSize {0};
     std::map<Key, std::unique_ptr<T> > _storedObjects;
     std::stack<std::unique_ptr<T> > _freeObjects;
 };
